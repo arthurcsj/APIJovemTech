d510e60d0b2551cd5822ea0913b7813b02e800f2
arthurcsj <arthurcleubersj@gmail.com>
2023-03-08 15:51:44 -0300

    mudança de cor

--- JovemTech/index.js
+++ JovemTech/index.js
@@ -1,38 +1,40 @@
 var express = require ("express");
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/home',function(req,res){
-  res.send('paguina inicial')
+  res.sendFile(__dirname + '/public/html/home.html');
 });
 
 
 app.get('/jovemtech/listaalunos',function(req,res){
-  res.send('Página para listar alunos matriculados')
+  res.sendFile(__dirname + '/public/html/alunos.html');
 });
 
 
 app.get('/jovemtech/professores',function(req,res){
-  res.send('Página para listar professores')
+  res.sendFile(__dirname + '/public/html/porfessores.html');
 });
 
 
 app.get('/jovemtech/cursos',function(req,res){
-  res.send('Página informar cursos ofertados')
+  res.sendFile(__dirname + '/public/html/cursos.html');
 });
 
 
 app.get('/jovemtech/inscricao',function(req,res){
-  res.send('Página de inscrição')
+  res.sendFile(__dirname + '/public/html/inscricao.html');
 });
 
 
 app.get('/jovemtech/comentarios',function(req,res){
-  res.send('Página para deixar seu comentário')
+  res.sendFile(__dirname + '/public/html/comentarios.html');
 });
 
 
 
-app.listen(8081,function(){
-    console.log("servidor rondando na porta http://localhost:8081/");
+app.listen(8080,function(){
+    console.log("servidor rondando na porta http://localhost:8080/home");
 
 });
